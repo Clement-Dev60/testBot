aa99b0f949ab8a13bee57455dd599b70a8365ee1
Clément Humez <zheum60@gmail.com>
2026-04-12 20:52:25 +0200

    fix: modification modèle ia gratuit

--- bot.py
+++ bot.py
@@ -205,7 +205,7 @@ async def on_message(message: discord.Message):
         async with message.channel.typing():
             try:
                 response = genai_client.models.generate_content(
-                    model="gemini-1.5-flash-latest", 
+                    model="gemini-2.0-flash-lite", 
                     config=types.GenerateContentConfig(
                         system_instruction=ALFRED_PROMPT
                     ),
